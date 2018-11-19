5bd98ca88b6d1c8a8a96c3c7c94f71412d172cca
Stephen Argentina <stephena9871@gmail.com>
2018-11-19 01:14:38 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -4,6 +4,7 @@ def get_first_name_of_season_winner(data, season)
     if key == "season 30"
       value.each{|dict|
         if dict["status"] == "Winner"
+          binding.pry
           return dict["name"]
         end
       }
